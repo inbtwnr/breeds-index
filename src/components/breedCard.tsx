62e44879cddbef4637febd07c709af1c8bb76128
o.prysiahnyi <o.prysiahnyi@alvacommerce.com>
2024-10-27 23:14:49 +0200

    change var declaration for image props

--- src/components/breedCard.tsx
+++ src/components/breedCard.tsx
@@ -8,7 +8,7 @@ interface BreedCardProps extends BreedType {
 }
 
 export default function BreedCard({id, name, image, species}: BreedCardProps) {
-    const { width, height, url } = image || {width: 0, height: 0, url: ''};
+    const { width, height, url } = image?.[0] || {width: 0, height: 0, url: ''};
 
     return (<Link href={`/breed/${species}/${id}`}>
         <Card className={'hover:opacity-80 flex-1 basis-80 lg:h-[380px] flex flex-col sm:max-w-[400px] lg:max-w-[336px] xl:max-w-[420px] overflow-hidden transition-all duration-100'} key={id}>
